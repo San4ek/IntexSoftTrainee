2d5a0441d9192735a0be5e37fa7e43bf2a3c0dcd
iNQU1SITOR <sankevich2003@mail.ru>
2024-05-28 22:28:12 +0300

    Кое-как работает

--- AuthService/src/main/java/me/inqu1sitor/authservice/Main.java
+++ AuthService/src/main/java/me/inqu1sitor/authservice/Main.java
@@ -3,6 +3,7 @@ package me.inqu1sitor.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
