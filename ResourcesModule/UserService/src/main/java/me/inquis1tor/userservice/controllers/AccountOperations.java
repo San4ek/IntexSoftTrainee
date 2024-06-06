d04bde433546f0d260a580e23d1bbd0e363ed4f0
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-06 10:03:07 +0300

    First try to add Swagger-doc to AccountOperations

--- ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/AccountOperations.java
+++ ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/AccountOperations.java
@@ -1,21 +1,31 @@
 package me.inquis1tor.userservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@Tag(name = "Account", description = "Account management APIs")
 @RequestMapping("/default")
 public interface AccountOperations {
 
     ResponseEntity<?> DEFAULT_RESPONSE=ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
 
+    @Operation(summary = "Get account by its email")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
     @GetMapping
     default ResponseEntity<?> get(@RequestParam String email) {
         return DEFAULT_RESPONSE;
     }
 
+    @Operation(summary = "Get account by its id")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
     @GetMapping
     default ResponseEntity<?> get(@RequestParam UUID id) {
         return DEFAULT_RESPONSE;
