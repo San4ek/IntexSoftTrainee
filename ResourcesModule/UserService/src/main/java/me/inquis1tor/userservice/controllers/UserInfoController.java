c90cc20f16408e39f39cc3fd111d166f60a88db0
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-09 16:14:03 +0300

    Created basic functions for user info controller

---
+++ ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/UserInfoController.java
@@ -0,0 +1,8 @@
+package me.inquis1tor.userservice.controllers;
+
+import me.inquis1tor.userservice.controllers.operations.UserInfoOperations;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserInfoController implements UserInfoOperations {
+}
