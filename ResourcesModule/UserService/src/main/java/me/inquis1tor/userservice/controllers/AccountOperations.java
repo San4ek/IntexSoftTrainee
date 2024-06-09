0899d23b1cc80ae43e818927d24511edb83b44c5
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-09 15:52:03 +0300

    Created basic functions for acccount controller

--- ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/AccountOperations.java
+++ ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/AccountOperations.java
@@ -3,6 +3,7 @@ package me.inquis1tor.userservice.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import me.inquis1tor.userservice.entities.Account;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,38 @@ public interface AccountOperations {
     @ApiResponse(responseCode = "200")
     @GetMapping("/id/{id}")
     default ResponseEntity<?> get(@PathVariable UUID id) {
+        return DEFAULT_RESPONSE;
+    }
+
+    @Operation(summary = "Delete account by ints id")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "501")
+    @DeleteMapping
+    default ResponseEntity<?> delete(@RequestParam UUID id) {
+        return DEFAULT_RESPONSE;
+    }
+
+    @Operation(summary = "Register new account")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "501")
+    @PostMapping("/register")
+    default ResponseEntity<?> register(/*@RequestBody CredentialsRequestDTO credentials*/) {
+        return DEFAULT_RESPONSE;
+    }
+
+    @Operation(summary = "Get all accounts")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "501")
+    @GetMapping("/all")
+    default ResponseEntity<?> getAll() {
+        return DEFAULT_RESPONSE;
+    }
+
+    @Operation(summary = "Change account status by its id")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "501")
+    @PutMapping("/update")
+    default ResponseEntity<?> changeStatus(@RequestParam UUID accountId, @RequestParam Account.Status status, @RequestParam UUID adminId) {
         return DEFAULT_RESPONSE;
     }
 }
