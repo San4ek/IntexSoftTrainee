b510fe4d4d4db19c57e0e0eec5709089b1e1c598
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-09 21:03:19 +0300

    Added resources (bug like a previous)

---
+++ ResourcesModule/UserService/src/main/java/resources/db/changelog/v.1.0.0/sql/07.06.24-init-table-personal_info.sql
@@ -0,0 +1,11 @@
+create table if not exists personal_info
+(
+    account_id   uuid         not null,
+    name         varchar(255),
+    patronymic   varchar(255),
+    phone_number varchar(255),
+    surname      varchar(255),
+    primary key (account_id),
+    constraint personal_info_account_fk
+        foreign key (account_id) references account
+);
