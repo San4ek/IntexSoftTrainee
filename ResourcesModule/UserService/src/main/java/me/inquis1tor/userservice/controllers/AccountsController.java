2ae5c869b7b845e80ad810f3934335607c0338ce
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-25 17:42:47 +0300

    US-1 Controllers, DTO, Swagger-doc realizations

---
+++ ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/controllers/AccountsController.java
@@ -0,0 +1,103 @@
+package me.inquis1tor.userservice.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import me.inquis1tor.userservice.annotations.security.IsAdmin;
+import me.inquis1tor.userservice.annotations.swagger.parameters.AccountIdParameter;
+import me.inquis1tor.userservice.annotations.swagger.parameters.AdminIdParameter;
+import me.inquis1tor.userservice.annotations.swagger.requests.SwaggerRequestBody;
+import me.inquis1tor.userservice.annotations.swagger.responses.*;
+import me.inquis1tor.userservice.annotations.swagger.statuses.OkResponseStatus;
+import me.inquis1tor.userservice.dtos.AccountDetailsTransferDto;
+import me.inquis1tor.userservice.dtos.AccountResponseDto;
+import me.inquis1tor.userservice.dtos.CredentialsRequestDto;
+import me.inquis1tor.userservice.exceptions.EndpointNotImplementedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Validated
+@Tag(name = "Account", description = "Account management APIs")
+@RequestMapping("/default/accounts")
+public interface AccountsController {
+
+    @AccountIdParameter
+    @Operation(summary = "Get account by its id")
+    @AccountDtoOkResponse
+    @NotFoundErrorResponse
+    @OkResponseStatus
+    @GetMapping
+    default @ResponseBody AccountResponseDto getAccount() throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @PreAuthorize("@securityService.hasCode('REGISTER_CODE')")
+    @Operation(summary = "Register new account")
+    @NoContentOkResponse
+    @ExpectationFailedErrorResponse
+    @BadRequestErrorResponse
+    @OkResponseStatus
+    @PostMapping
+    default void registerAccount(@SwaggerRequestBody(description = "Account details")
+                                     @RequestBody AccountDetailsTransferDto account) throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @IsAdmin
+    @Operation(summary = "Get all accounts")
+    @AccountDtoArrayOkResponse
+    @OkResponseStatus
+    @GetMapping("/all")
+    default @ResponseBody Iterable<AccountResponseDto> getAllAccounts() throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @PreAuthorize("@securityService.hasCode('BLOCK_CODE')")
+    @AccountIdParameter
+    @AdminIdParameter
+    @Operation(summary = "Block account by its id")
+    @AccountDtoOkResponse
+    @BadRequestErrorResponse
+    @OkResponseStatus
+    @PutMapping("/block")
+    default void blockAccount(@RequestParam UUID accountId, @RequestParam UUID adminId) throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @PreAuthorize("@securityService.hasCode('UNBLOCK_CODE')")
+    @AccountIdParameter
+    @Operation(summary = "Unblock account by its id")
+    @AccountDtoOkResponse
+    @BadRequestErrorResponse
+    @OkResponseStatus
+    @PutMapping("/unblock")
+    default void unblockAccount(@RequestParam UUID accountId) throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @PreAuthorize("@securityService.hasCode('UNBLOCK_CODE')")
+    @AccountIdParameter
+    @Operation(summary = "Unblock account by its id")
+    @AccountDtoOkResponse
+    @BadRequestErrorResponse
+    @OkResponseStatus
+    @PutMapping("/email")
+    default void updateEmail(@RequestParam UUID accountId) throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+
+    @PreAuthorize("@securityService.hasCode('EMAIL_CODE')")
+    @AccountIdParameter
+    @Operation(summary = "Update account email by account id")
+    @NoContentOkResponse
+    @ExpectationFailedErrorResponse
+    @BadRequestErrorResponse
+    @PutMapping("/email")
+    default void updateEmail(@RequestParam UUID accountId,@SwaggerRequestBody(description = "Account credentials")
+                                   @Valid @RequestBody CredentialsRequestDto credentials) throws EndpointNotImplementedException {
+        throw new EndpointNotImplementedException();
+    }
+}
