03bfc46c9a9783f2a4090b44eaba3321decf5abe
iNQU1SITOR <sankevich2003@mail.ru>
2024-06-26 11:10:47 +0300

    Updated DTOs and controllers

--- ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/dtos/CredentialsResponseDto.java
+++
@@ -1,9 +0,0 @@
-package me.inquis1tor.userservice.dtos;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "Credentials information model")
-public record CredentialsResponseDto(
-        @Schema(description = "Account email", example = "sankevich2003@mail.ru")
-        String email
-) {}
