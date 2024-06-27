912766186ac10a47ed8dff7dea86171ea8b3a1e9
Eugene Yuntsevich <uncevichzena@gmail.com>
2024-06-27 17:50:12 +0300

    SS-3 Creating controllers for stock service

---
+++ ResourcesModule/StockService/src/main/java/org/example/controllers/StockModeratorOperations.java
@@ -0,0 +1,53 @@
+package org.example.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.SneakyThrows;
+import org.example.dtos.StockItemRequest;
+import org.example.dtos.StockItemResponse;
+import org.example.exceptions.EndpointNotImplementedException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Tag(name = "ModeratorStock", description = "Moderator's stock APIs")
+@RequestMapping("/stock")
+public interface StockModeratorOperations {
+
+    @SneakyThrows
+    @Operation(summary = "Get product by id")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @GetMapping("/find/{stockItemId}")
+    default @ResponseBody StockItemResponse getStockItemById(@PathVariable UUID stockItemId) {
+        throw new EndpointNotImplementedException();
+    }
+
+    @SneakyThrows
+    @Operation(summary = "Create a new product")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @PostMapping("/create")
+    default @ResponseBody StockItemResponse createStockItem(@RequestBody StockItemRequest stockItemDto) {
+        throw new EndpointNotImplementedException();
+    }
+
+    @SneakyThrows
+    @Operation(summary = "Update existing product")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @PutMapping("/update/{stockItemId}")
+    default @ResponseBody StockItemResponse updateStockItem(@PathVariable UUID stockItemId, @RequestBody StockItemRequest stockItemDto) {
+        throw new EndpointNotImplementedException();
+    }
+
+    @SneakyThrows
+    @Operation(summary = "Delete product")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @DeleteMapping("/delete/{stockItemId}")
+    default void deleteStockItem(@PathVariable UUID stockItemId) {
+        throw new EndpointNotImplementedException();
+    }
+}
---
+++ ResourcesModule/StockService/src/main/java/org/example/controllers/StockUserOperations.java
@@ -0,0 +1,53 @@
+package org.example.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.SneakyThrows;
+import org.example.dtos.StockItemRequest;
+import org.example.dtos.StockItemResponse;
+import org.example.enums.ColorEnum;
+import org.example.enums.SizeEnum;
+import org.example.enums.TypeEnum;
+import org.example.exceptions.EndpointNotImplementedException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Tag(name = "UserStock", description = "User's stock APIs")
+@RequestMapping("/user-stock")
+public interface StockUserOperations {
+
+    @SneakyThrows
+    @Operation(summary = "Get all products in stock")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @GetMapping("/find/all")
+    default List<StockItemResponse> getAllStockItems() {
+        throw new EndpointNotImplementedException();
+    }
+
+    @SneakyThrows
+    @Operation(summary = "Get product by name")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @GetMapping("/find/{name}")
+    default @ResponseBody List<StockItemResponse> getStockItemsByName(@PathVariable String name) {
+        throw new EndpointNotImplementedException();
+    }
+
+    @SneakyThrows
+    @Operation(summary = "Get products by attributes")
+    @ApiResponse(responseCode = "501")
+    @ApiResponse(responseCode = "200")
+    @GetMapping("/find/attributes")
+    default List<StockItemResponse> getStockItemsByAttributes(@RequestParam(required = false) String brand,
+                                                              @RequestParam(required = false) ColorEnum color,
+                                                              @RequestParam(required = false) SizeEnum size,
+                                                              @RequestParam(required = false) TypeEnum type,
+                                                              @RequestParam(required = false) Float minPrice,
+                                                              @RequestParam(required = false) Float maxPrice) {
+        throw new EndpointNotImplementedException();
+    }
+}
