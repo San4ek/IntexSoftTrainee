a763beb61c2c093cd5b99a921118338bfa8afa45
Eugene Yuntsevich <uncevichzena@gmail.com>
2024-06-28 23:08:53 +0300

    SS-9 Add logs in exceptions

--- ResourcesModule/StockService/src/main/java/org/example/exceptions/EndpointNotImplementedException.java
+++ ResourcesModule/StockService/src/main/java/org/example/exceptions/EndpointNotImplementedException.java
@@ -1,7 +1,11 @@
 package org.example.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class EndpointNotImplementedException extends Exception {
     public EndpointNotImplementedException() {
         super("Endpoint not implemented");
+        log.error("Endpoint not implemented");
     }
 }
