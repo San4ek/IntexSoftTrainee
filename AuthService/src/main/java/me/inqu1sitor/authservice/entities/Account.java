a8a7d245a8652eef0f012d6195fa7f8be8c8d412
iNQU1SITOR <sankevich2003@mail.ru>
2024-07-04 17:45:58 +0300

    AS-2 Entities, database migrations

---
+++ AuthService/src/main/java/me/inqu1sitor/authservice/entities/Account.java
@@ -0,0 +1,73 @@
+package me.inqu1sitor.authservice.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "account")
+public class Account implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.id.toString();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !status.equals(Status.BLOCKED);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !status.equals(Status.DELETED);
+    }
+
+    public enum Role {
+        USER,
+        MODER,
+        ADMIN
+    }
+
+    public enum Status {
+        ACTIVE,
+        DELETED,
+        BLOCKED
+    }
+}
