42929965ec1584faf8cfa8275b859c3e8dabf9d4
iNQU1SITOR <sankevich2003@mail.ru>
2024-07-04 17:38:12 +0300

    AS-1 Controllers and DTO, Swagger-doc

---
+++ AuthService/src/main/java/me/inqu1sitor/authservice/annotations/swagger/responses/BadRequestErrorResponse.java
@@ -0,0 +1,21 @@
+package me.inqu1sitor.authservice.annotations.swagger.responses;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import me.inqu1sitor.authservice.dtos.ErrorResponseDto;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Target(METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "400",
+        content = @Content(schema = @Schema(implementation = ErrorResponseDto.class),
+                mediaType = MediaType.APPLICATION_JSON_VALUE))
+public @interface BadRequestErrorResponse {
+}
