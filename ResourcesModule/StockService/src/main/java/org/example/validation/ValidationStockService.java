83577d9187591a59c31830bad05f6cf5bd6b2199
Eugene Yuntsevich <uncevichzena@gmail.com>
2024-07-10 18:44:03 +0300

    SS-11 Fix validation services

--- ResourcesModule/StockService/src/main/java/org/example/validation/ValidationStockService.java
+++ ResourcesModule/StockService/src/main/java/org/example/validation/ValidationStockService.java
@@ -3,7 +3,6 @@ package org.example.validation;
 import lombok.RequiredArgsConstructor;
 import org.example.dtos.StockItemRequest;
 import org.example.entities.ProductEntity;
-import org.example.exceptions.ProductNotExistException;
 import org.example.repositories.ProductRepository;
 import org.example.repositories.StockRepository;
 import org.springframework.stereotype.Service;
@@ -14,6 +13,9 @@ import java.util.UUID;
 import static org.example.utils.validation.ValidatorUtils.checkFalse;
 import static org.example.utils.validation.ValidatorUtils.checkTrue;
 
+/**
+ * Class for validating stock service operations
+ */
 @Service
 @RequiredArgsConstructor
 public class ValidationStockService {
@@ -21,12 +23,23 @@ public class ValidationStockService {
     private final ProductRepository productRepository;
     private final StockRepository stockRepository;
 
+    /**
+     * Validates a stock item creation request.
+     *
+     * @param stockItemRequest The request containing details of the stock item to be created.
+     */
     @Transactional(readOnly = true)
     public void validateStockItemForCreate(final StockItemRequest stockItemRequest) {
         final ProductEntity productEntity = productRepository.getById(stockItemRequest.getProductId());
         checkFalse(stockRepository.existsByColorAndSizeAndProduct(stockItemRequest.getColor(), stockItemRequest.getSize(), productEntity), "Stock item exists with such parameters");
     }
 
+    /**
+     * Validates a stock item update request.
+     *
+     * @param stockItemId The ID of the stock item to be updated.
+     * @param stockItemRequest The updated details of the stock item.
+     */
     @Transactional(readOnly = true)
     public void validateStockItemForUpdate(final UUID stockItemId, final StockItemRequest stockItemRequest) {
         checkTrue(stockRepository.existsById(stockItemId), "Stock doesn't exist with id: " + stockItemId);
@@ -34,6 +47,11 @@ public class ValidationStockService {
         checkFalse(stockRepository.existsByColorAndSizeAndProduct(stockItemRequest.getColor(), stockItemRequest.getSize(), productEntity), "Stock item exists with such parameters");
     }
 
+    /**
+     * Validates a stock item deletion request.
+     *
+     * @param stockItemId The ID of the stock item to be deleted.
+     */
     @Transactional(readOnly = true)
     public void validateStockItemForDelete(final UUID stockItemId) {
         checkTrue(stockRepository.existsByIdAndAmountEquals(stockItemId, 0), "Stock item must exist and amount must be 0");
