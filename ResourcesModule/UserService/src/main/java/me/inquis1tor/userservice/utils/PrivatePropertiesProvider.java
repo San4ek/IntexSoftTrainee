f2ce27171280a65eccf0c4465f8800800beb302f
iNQU1SITOR <sankevich2003@mail.ru>
2024-07-11 22:05:54 +0300

    Updated utils

---
+++ ResourcesModule/UserService/src/main/java/me/inquis1tor/userservice/utils/PrivatePropertiesProvider.java
@@ -0,0 +1,11 @@
+package me.inquis1tor.userservice.utils;
+
+/**
+ * Implementations of this interface are responsible for providing the required private properties.
+ *
+ * @author Alexander Sankevich
+ */
+public interface PrivatePropertiesProvider {
+
+    String getAuthCode();
+}
