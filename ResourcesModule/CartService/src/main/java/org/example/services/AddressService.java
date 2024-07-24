1f7debb1b57d97079ffa66103fa5c2d197ded77b
Eugene Yuntsevich <uncevichzena@gmail.com>
2024-07-24 12:41:40 +0300

    CS-9 Add javadocs

--- ResourcesModule/CartService/src/main/java/org/example/services/AddressService.java
+++ ResourcesModule/CartService/src/main/java/org/example/services/AddressService.java
@@ -6,6 +6,9 @@ import org.example.entities.AddressEntity;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Interface for addresses
+ */
 public interface AddressService {
 
     List<AddressEntity> getAddresses();
--- ResourcesModule/CartService/src/main/java/org/example/services/impl/CurrencyConversionServiceImpl.java
+++ ResourcesModule/CartService/src/main/java/org/example/services/impl/CurrencyConversionServiceImpl.java
@@ -1,12 +1,12 @@
 package org.example.services.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dtos.CbrCurrencyResponse;
 import org.example.dtos.CurrencyRateResponse;
+import org.example.exceptions.InvalidObjectException;
 import org.example.services.CurrencyConversionService;
 import org.example.services.client.CbrCurrencyClient;
 import org.springframework.cache.annotation.CacheEvict;
@@ -17,6 +17,10 @@ import java.util.Optional;
 
 import static org.example.utils.validation.ValidatorUtils.checkTrue;
 
+/**
+ * Service for retrieving currency rates from Central Bank of Russia in xml format
+ * and converting currencies using this rates.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -24,24 +28,40 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 
     private final CbrCurrencyClient currencyClient;
 
+    /**
+     * Retrieves xml file from currency client in String format, convert it into dto,
+     * retrieves currency rates from this dto, and converts amount into another currency.
+     *
+     * @param fromCurrency Currency to convert from.
+     * @param toCurrency Currency to convert into.
+     * @param amount Amount of currency to convert
+     * @return converted amount of currency.
+     */
     @SneakyThrows
     @Override
     @Cacheable("currencyRates")
     public Double convertCurrency(final String fromCurrency, final String toCurrency, final Double amount) {
-        String xmlResponse = currencyClient.getDailyRates();
-        xmlResponse = xmlResponse.replace(',', '.');
-        XmlMapper xmlMapper = new XmlMapper();
-        CbrCurrencyResponse response = xmlMapper.readValue(xmlResponse, CbrCurrencyResponse.class);
-        Optional<CurrencyRateResponse> fromRate = response.getCurrencyRates().stream()
-                .filter(rate -> rate.getCharCode().equals(fromCurrency)).findFirst();
-        Optional<CurrencyRateResponse> toRate = response.getCurrencyRates().stream()
-                .filter(rate -> rate.getCharCode().equals(toCurrency)).findFirst();
-        checkTrue(fromRate.isPresent() && toRate.isPresent(), "Invalid currency rate");
-        Double fromValue = fromRate.get().getValue() / fromRate.get().getNominal();
-        Double toValue = toRate.get().getValue() / fromRate.get().getNominal();
-        return amount * (fromValue / toValue);
+        try {
+            String xmlResponse = currencyClient.getDailyRates();
+            xmlResponse = xmlResponse.replace(',', '.');
+            XmlMapper xmlMapper = new XmlMapper();
+            CbrCurrencyResponse response = xmlMapper.readValue(xmlResponse, CbrCurrencyResponse.class);
+            Optional<CurrencyRateResponse> fromRate = response.getCurrencyRates().stream()
+                    .filter(rate -> rate.getCharCode().equals(fromCurrency)).findFirst();
+            Optional<CurrencyRateResponse> toRate = response.getCurrencyRates().stream()
+                    .filter(rate -> rate.getCharCode().equals(toCurrency)).findFirst();
+            checkTrue(fromRate.isPresent() && toRate.isPresent(), "Invalid currency rate");
+            Double fromValue = fromRate.get().getValue() / fromRate.get().getNominal();
+            Double toValue = toRate.get().getValue() / fromRate.get().getNominal();
+            return amount * (fromValue / toValue);
+        } catch (Exception e) {
+            throw new InvalidObjectException("Failed to convert currencies.");
+        }
     }
 
+    /**
+     * Method for clearing cache
+     */
     @Override
     @CacheEvict("currencyRates")
     public void clearCache() {
